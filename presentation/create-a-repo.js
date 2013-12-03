853064ef8ea50a596ad156a1a1cb8c02b1828f04
Todd Wolfson <todd@twolfson.com>
2013-12-03 00:25:38 -0800

    Adding help dialog to creation of a repo

--- presentation/create-a-repo.js
+++ presentation/create-a-repo.js
@@ -7,18 +7,25 @@ module.exports = function () {
   // TODO: Code up a HELP action
   console.log(center('Create a repo'.underline));
   console.log('');
-  console.log('# Open a new terminal window');
-  console.log('cd ~/github');
-  console.log('mkdir hai-node-summit');
-  console.log('cd hai-node-summit');
-  console.log('git init');
-  console.log('');
-  console.log('hub create');
-  console.log('');
-  console.log('grunt-init node');
-  console.log('');
-  console.log('subl .');
+  if (!process.env.HELP) {
+    console.log('# Open a new terminal window');
+    console.log('cd ~/github');
+    console.log('mkdir hai-node-summit');
+    console.log('cd hai-node-summit');
+    console.log('git init');
+    console.log('');
+    console.log('hub create');
+    console.log('');
+    console.log('grunt-init node');
+    console.log('');
+    console.log('subl .');
 
-  // Place some vertical padding
-  console.log('');
+    // Place some vertical padding
+    console.log('');
+  } else {
+    console.log('cd ~/github/twolfson.com');
+    console.log('npm start');
+    console.log('firefox http://127.0.0.1:8080/2013-07-27-develop-faster#starting-a-repo');
+    console.log('');
+  }
 };
