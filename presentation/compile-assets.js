514cece7163f7ac4527cd488a7b74ff02b3606e3
Todd Wolfson <todd@twolfson.com>
2013-12-03 02:27:28 -0800

    Opting for solution within twolfson.com since it lighter and the intent is more about the result than the process

--- presentation/compile-assets.js
+++ presentation/compile-assets.js
@@ -4,39 +4,11 @@ var functionToString = require('function-to-string');
 
 module.exports = function () {
   console.log(center('Compile assets'.underline));
-
-function code() {/*
-# Inside of terminal
-lnpm grunt-less --save-dev
-grunt less watch
-
-# Inside of Gruntfile.js
-module.exports = function (grunt) {
-  grunt.initConfig({
-    less: {
-      files: [{
-        src: 'public/index.less',
-        dest: 'public/index.css'
-      }]
-    },
-    watch: {
-      less: {
-        files: '<%= less.files.src %>',
-        tasks: ['less']
-      }
-    }
-  });
-
-  grunt.loadNpmTask('grunt-less');
-  grunt.registerTask('default', ['less']);
-};
-
-# Inside of `public/index.less`
-@background-image: 'grid.png';
-
-body {
-  background: url(@background-image);
-}
-*/}
-  console.log(functionToString(code).body.replace('/*', '').replace('*/', ''));
+  console.log('');
+  console.log('cd ~/github/twolfson.com');
+  console.log('grunt sass watch');
+  console.log('');
+  console.log('# In another terminal');
+  console.log('pico public/css/vars.scss');
+  console.log('');
 };
